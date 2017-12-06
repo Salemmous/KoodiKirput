d42b02e9ae8603d84f68300e6086324090679709
Salemmous <thomas.debluts@gmail.com>
2017-12-06 20:36:34 +0200

    #17 API returning the suggestions in required form

--- SomaliKirppu/API/views.py
+++ SomaliKirppu/API/views.py
@@ -11,8 +11,12 @@ def suggest(request, word):
     response = r.json();
     suggestions = [];
     for doc in response['response']['docs']:
-        suggestions.append(doc['en'][0]);
-    return HttpResponse(json.dumps({'suggestions': suggestions}, indent=4))
+        word = {'id':'', 'text':''};
+        word['id'] = doc['en'][0];
+        word['text'] = doc['so'][0];
+        suggestions.append(word);
+    #return HttpResponse(json.dumps({'suggestions': suggestions}, indent=4))
+    return HttpResponse(json.dumps(suggestions, indent=4))
 
 def translation(request, word):
     template = loader.get_template('translation.html')
@@ -22,10 +26,6 @@ def translation(request, word):
     return HttpResponse(template.render(context, request))
 def edit(request, id, en, so):
     editstr = '[{ "id" : "' + id + '", "en" : {"set":["' + en + '"]}, "so" : {"set":["' + so + '"]}}]'
-    enArr = []
-    enArr.append(en)
-    soArr = []
-    soArr.append(so)
     r = requests.post("http://localhost:8983/solr/somali/update", data = editstr,
                       headers={'Content-type': 'application/json'})
     r2 = requests.post("http://localhost:8983/solr/somali/update", data = '<commit />',
